1b95e9f38f9498acfef95a7533cb3cd34a78ae63
Jurriaan Barkey Wolf <jjhbarkeywolf@gmail.com>
2018-03-16 13:39:19 +0100

    Added additional test for Problem.ToSolveable

--- api_test.go
+++ api_test.go
@@ -29,7 +29,8 @@ func TestProblem_checkExpression(t *testing.T) {
 
 }
 
-func TestProblem_toSolveable(t *testing.T) {
+// a simple case with one inequality and no integrality constraints
+func TestProblem_toSolveableA(t *testing.T) {
 
 	// build an abstract Problem
 	prob := NewProblem()
@@ -90,6 +91,61 @@ func TestProblem_toSolveable(t *testing.T) {
 		}),
 		h: []float64{2},
 		integralityConstraints: []bool{false, false, false, false},
+	}
+
+	//Note:  do not compare pointers
+	assert.Equal(t, expected, *solveable)
+}
+
+// No inequalities and 2 integrality constraints
+func TestProblem_toSolveableB(t *testing.T) {
+
+	// build an abstract Problem
+	prob := NewProblem()
+
+	// add the variables
+	v1 := prob.AddVariable(-1, false)
+	v2 := prob.AddVariable(-2, true)
+	v3 := prob.AddVariable(1, true)
+
+	// add the equality constraints
+	prob.AddEquality([]expression{
+		expression{
+			coef:     1,
+			variable: v1,
+		},
+	},
+		5,
+	)
+	prob.AddEquality([]expression{
+		expression{
+			coef:     3,
+			variable: v2,
+		},
+	},
+		2,
+	)
+	prob.AddEquality([]expression{
+		expression{
+			coef:     1,
+			variable: v3,
+		},
+	},
+		2,
+	)
+
+	solveable := prob.toSolveable()
+	expected := MILPproblem{
+		c: []float64{-1, -2, 1},
+		A: mat.NewDense(3, 3, []float64{
+			1, 0, 0,
+			0, 3, 0,
+			0, 0, 1,
+		}),
+		b: []float64{5, 2, 2},
+		G: nil,
+		h: nil,
+		integralityConstraints: []bool{false, true, true},
 	}
 
 	//Note:  do not compare pointers
